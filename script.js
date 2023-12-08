34563dae4420b4ffe35074f3b3e0c01532f817a2
kednaleal <capic.2023118tads0017@aluno.ifpi.edu.br>
2023-12-07 21:47:46 -0300

    feat: desenvolvendo os campos de entrada

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-var screen = document.querySelector('#screen');
-var btn = document.querySelectorAll('.btn');
+function calcularForcaEletrica() {
+    const k = 9e9; // constante eletrostática em N m^2/C^2
 
-for(item of btn){
-    item.addEventListener('click', (e)=>{
-        btntext=e.target.innerText;
-        screen.value+=btntext;
-    });
-}
+    const q1 = parseFloat(document.getElementById("q1").value);
+    const q2 = parseFloat(document.getElementById("q2").value);
+    const distance = parseFloat(document.getElementById("distance").value);
+
+    const force = (k * Math.abs(q1) * Math.abs(q2)) / Math.pow(distance, 2);
+
+    document.getElementById("result").innerText = `Força Elétrica: ${force.toExponential(2)} N`;
+  }
