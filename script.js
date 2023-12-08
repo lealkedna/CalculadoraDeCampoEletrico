533e03fb5b85383a8423b2798cfad607ed5bcc68
kednaleal <capic.2023118tads0017@aluno.ifpi.edu.br>
2023-12-08 19:30:49 -0300

    atualização do resultado e inserido a fórmula

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function calcularForcaEletrica() {
-    const k = 9e9; // constante eletrostática em N m^2/C^2
+  const k = 9e9; // constante eletrostática em N m^2/C^2
 
-    const q1 = parseFloat(document.getElementById("q1").value);
-    const q2 = parseFloat(document.getElementById("q2").value);
-    const distance = parseFloat(document.getElementById("distance").value);
+  const q1 = parseFloat(document.getElementById("q1").value);
+  const q2 = parseFloat(document.getElementById("q2").value);
+  const distance = parseFloat(document.getElementById("distance").value);
 
-    const force = (k * Math.abs(q1) * Math.abs(q2)) / Math.pow(distance, 2);
+  const force = (k * Math.abs(q1) * Math.abs(q2)) / Math.pow(distance, 2);
 
-    document.getElementById("result").innerText = `Força Elétrica: ${force.toExponential(2)} N`;
-  }
+  document.getElementById("result").innerText = `Força Elétrica: ${force.toExponential(2)} N`;
+}
